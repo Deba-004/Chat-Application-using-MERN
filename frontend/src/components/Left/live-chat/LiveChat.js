c2d4ad770af1230870b4989f230e679e99da1f7a
Debasish Sarkar <debasishsarkar452@gmail.com>
2025-07-09 20:54:35 +0530

    modified

--- frontend/src/components/Left/live-chat/LiveChat.js
+++ frontend/src/components/Left/live-chat/LiveChat.js
@@ -1,6 +1,6 @@
 function LiveChat() {
     return (
-        <div className="bg-[#0c1017">
+        <div className="bg-[#0c1017]">
             <h3 className="font-bold text-center hover:text-gray-200 my-2 cursor-pointer">Live Chat</h3>
         </div>
     );
