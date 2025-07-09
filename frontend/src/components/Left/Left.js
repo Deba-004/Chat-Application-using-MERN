855e5ebb79da34898a14e6525fe2bad455a6e95c
Debasish Sarkar <debasishsarkar452@gmail.com>
2025-07-09 20:38:44 +0530

    modified

--- frontend/src/components/Left/Left.js
+++ frontend/src/components/Left/Left.js
@@ -3,6 +3,7 @@ import classes from "./index.module.css";
 import LeftHeader from "./left-header/LeftHeader";
 import Search from "./search/Search";
 import Users from "./users/Users";
+import LiveChat from "./live-chat/LiveChat";
 
 function Left() {
     const [searchInput, setSearchInput] = useState("");
@@ -12,6 +13,7 @@ function Left() {
             <LeftHeader />
             <Search searchInput={searchInput} setSearchInput={setSearchInput} />
             <Users searchInput={searchInput} />
+            <LiveChat />
         </div>
     );
 }
---
+++ frontend/src/components/Left/live-chat/LiveChat.js
@@ -0,0 +1,9 @@
+function LiveChat() {
+    return (
+        <div>
+            <h3 className="text-2xl font-bold text-center mt-4 hover:text-gray-200">Live Chat</h3>
+        </div>
+    );
+}
+
+export default LiveChat;
